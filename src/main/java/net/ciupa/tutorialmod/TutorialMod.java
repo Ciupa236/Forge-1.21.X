2d3b17b19cf7f69504c6b1126477950800ab81f8
Ciupa236 <cbasoc2009@gmail.com>
2024-11-19 19:52:54 +0200

    added items! :D

--- src/main/java/net/ciupa/tutorialmod/TutorialMod.java
+++ src/main/java/net/ciupa/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.ciupa.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.ciupa.tutorialmod.block.ModBlocks;
 import net.ciupa.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -32,6 +33,7 @@ public class TutorialMod {
         MinecraftForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -58,6 +60,10 @@ public class TutorialMod {
         }
         if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(ModItems.BABY_OIL);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
     }
 
