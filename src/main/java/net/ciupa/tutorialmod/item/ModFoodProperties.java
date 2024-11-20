a5aa9d92b2425e65bae85a0ecd4447b17288fa40
Ciupa236 <cbasoc2009@gmail.com>
2024-11-20 20:26:28 +0200

    added items! :D

--- src/main/java/net/ciupa/tutorialmod/item/ModFoodProperties.java
+++ src/main/java/net/ciupa/tutorialmod/item/ModFoodProperties.java
@@ -8,8 +8,8 @@ public class ModFoodProperties {
     public static final FoodProperties KOHLRABI = new FoodProperties.Builder().nutrition(3).saturationModifier(0.25f)
             .effect(new MobEffectInstance(MobEffects.INVISIBILITY, 400), 0.20f).build();
     public static final FoodProperties DOG_TREAT = new FoodProperties.Builder().nutrition(3).saturationModifier(0.25f)
-            .effect(new MobEffectInstance(MobEffects.REGENERATION, 600, 0), 1.0f)
-            .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 0), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.REGENERATION, 300, 1), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1200, 1), 1.0f)
             .alwaysEdible()
             .build();
 }
