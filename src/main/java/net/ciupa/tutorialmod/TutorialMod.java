dd77f8b305e086afdf99393c3268a3e8ac219bef
Ciupa236 <cbasoc2009@gmail.com>
2024-11-20 17:40:38 +0200

    added items! :D

--- src/main/java/net/ciupa/tutorialmod/TutorialMod.java
+++ src/main/java/net/ciupa/tutorialmod/TutorialMod.java
@@ -63,6 +63,7 @@ public class TutorialMod {
         }
         if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(ModItems.BABY_OIL);
+            event.accept(ModItems.CHISEL);
         }
         if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
             event.accept(ModBlocks.ALEXANDRITE_BLOCK);
